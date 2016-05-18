ee3eee407a632c3fd3153792f337ba89b6e832d2
romanornr <romanornr@gmail.com>
2016-05-18 22:23:28 +0200

    Player API controller

--- app/Http/Controllers/PlayerController.php
+++ app/Http/Controllers/PlayerController.php
@@ -18,22 +18,31 @@ class PlayerController extends Controller
 
     public function getPlayer($id)
     {
-
+        $player = Players::find($id);
+        return response()->json($player);
     }
 
     public function createPlayer(Request $request)
     {
-
+        $player = Players::create($request->all());
+        return reponse()->json($player);
     }
 
     public function deletePlayer($id)
     {
-
+        $player = Player::find($id);
+        $player->delete();
+        return response()->json('player deleted');
     }
 
     public function updatePlayer(Request $request, $id)
     {
-        
+        $player = Players::find($id);
+        $player->username = $request->input('username');
+        $player->score = $request->input('score');
+        $player->save();
+
+        return response()->json($player);
     }
 
 }
