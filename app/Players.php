aa88828e02fb801bc4400882be90f2d6c55ac923
romanornr <romanornr@gmail.com>
2016-05-18 12:42:00 +0200

    Added Players model

---
+++ app/Players.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App;
+
+use illuminate\Database\Eloquent\Model;
+
+class Players extends Model
+{
+	/**
+	 * table from players
+	 * @var
+	 */
+	protected $table = 'Players';
+
+	/**
+	 * guard id, created_at and updated_at
+	 * protection for mass assignment vulnerability.
+	 * @var
+	 */
+	protected $guarded = ['id', 'created_at', 'updated_at'];
+
+	/**
+	 * put $dates in to Carbon instances for date mutation. 
+	 * @var
+	 */
+	protected $dates = ['created_at', 'updated_at'];
+}
